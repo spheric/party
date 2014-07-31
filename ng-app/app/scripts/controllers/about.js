503d2c42ac25e5958a7c203cbcebda91b342dd51
Matthew Gardner <spheric89@gmail.com>
2014-07-31 23:45:54 +1000

    added yeoman angular scaffold

---
+++ ng-app/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name partyocracyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the partyocracyApp
+ */
+angular.module('partyocracyApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ng-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name partyocracyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the partyocracyApp
+ */
+angular.module('partyocracyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
